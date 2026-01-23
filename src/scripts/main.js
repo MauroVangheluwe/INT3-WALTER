bd9404fbbc7a786538dc3a2cd66f6492f54e3ed6
Mauro Vangheluwe <mauro.vangheluwe@student.howest.be>
2026-01-23 15:14:12 +0100

    header placement grid + beginning intro section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css'
 import $ from 'jquery'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { initGunInteraction } from './gunInteraction.js'
 
 // GSAP Plugins registreren
 gsap.registerPlugin(ScrollTrigger);
@@ -12,31 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // JavaScript-dependent stijl
   document.documentElement.classList.add('js-enabled');
 
-  // Track click count for progressive blur reveal
-  let clickCount = 0;
-  const maxClicks = 4;
-  
-  const gun = document.querySelector('.gun');
-  const gunShape = document.querySelector('.gun-shape');
-
-  if (gun && gunShape) {
-    gun.addEventListener('click', () => {
-      // Stop animation on first click
-      gun.classList.add('clicked');
-      
-      // Trigger recoil on Gun
-      gun.classList.add('recoil');
-      setTimeout(() => gun.classList.remove('recoil'), 200);
-
-      // Trigger Gentle Shake
-      document.body.classList.add('shake');
-      setTimeout(() => document.body.classList.remove('shake'), 200);
-      
-      if (clickCount < maxClicks) {
-        clickCount++;
-        gunShape.setAttribute('data-reveal', clickCount);
-        console.log(`Gun clicked: ${clickCount}/${maxClicks}`);
-      }
-    });
-  }
+  // Initialize gun interaction
+  initGunInteraction();
 });
